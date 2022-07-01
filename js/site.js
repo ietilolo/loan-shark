0c527491b45ab4655809f1a8e4448f92668f12df
Ieti Lolo <31405843+ietilolo@users.noreply.github.com>
2022-07-01 13:44:40 +1200

    updated app

--- js/site.js
+++ js/site.js
@@ -1,5 +1,30 @@
 
-// Call hello
-function HelloWorld(){
-  alert("Hello, world!");
+// Get all values
+function getValues(){
+  let loanAmount = parseInt(document.getElementById("loanAmount").value);
+  let loanMonths = parseInt(document.getElementById("loanMonths").value);
+  let loanRate = parseFloat(document.getElementById("loanRate").value).toFixed(2);
+
+  // Call generate data
+  let data = generateData(loanAmount, loanMonths, parseFloat(loanRate));
+
+  // Call displayresults
+}
+
+// Generate Data based on values
+function generateData(loanAmount, loanMonths, loanRate){
+  // Find Total monthy payment
+  let totalMonthlyPayment = (loanAmount * (loanRate/1200)) / 1 - (1 + loanRate/1200) - loanMonths;
+  // Find remaining balance
+  let totalRemain = loanAmount;
+  // Find interest payment
+  let totalInterest = totalRemain * (loanRate/1200);
+  // Find principle payment
+  let totalPrincipal = totalMonthlyPayment - totalInterest;
+  console.log(totalMonthlyPayment);
+}
+
+// Display results
+function displayResults(){
+
 }
